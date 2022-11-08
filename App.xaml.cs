9ffa0894d15df85b4d472784f349e47802d698ba
Joshua Foster <josh2112@gmail.com>
2022-11-08 13:12:22 -0500

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,98 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Hardcodet.Wpf.TaskbarNotification;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using System.Windows;
+using System.Text.Json;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Media.Animation;
+
+namespace ChromeWindowFix
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private TaskbarIcon? taskbarIcon;
+
+        public RelayCommand ExitCommand { get; }
+
+        private WindowPositionFixer? windowPositionFixer;
+
+        public App()
+        {
+            ExitCommand = new RelayCommand( Shutdown );
+
+            var singleInstanceHelper = new SingleInstanceHelper( "{64f42670-5f7b-11ed-9b6a-0242ac120002}",
+                () => { }, () => Shutdown() );
+
+            Exit += ( s, e ) => singleInstanceHelper.Dispose();
+        }
+        protected override void OnStartup( StartupEventArgs e )
+        {
+            base.OnStartup( e );
+
+            taskbarIcon = (TaskbarIcon)FindResource( "taskbarIcon" );
+            taskbarIcon.DataContext = this;
+
+            var config = JsonSerializerExtensions.DeserializeAnonymousType( File.ReadAllText( "config.json" ),
+                new { adjust = new { top = 0 } } );
+
+            if( config == null )
+            {
+                MessageBox.Show( "Unable to read config file" );
+                Shutdown();
+                return;
+            }
+
+            windowPositionFixer = new WindowPositionFixer(
+                p => p.MainWindowTitle.Contains( "Chrome" ) && p.ProcessName.Contains( "chrome" ),
+                ( windowRect, desktopRect ) => windowRect.Height == desktopRect.Height && windowRect.Width != desktopRect.Width && windowRect.Top == 0,
+                r => new System.Drawing.Rectangle( r.X, r.Y + config.adjust.top, r.Width, r.Height - config.adjust.top ));
+        }
+    }
+
+
+    public class SingleInstanceHelper
+    {
+        public class AnotherInstanceOpenException : Exception { }
+
+        private EventWaitHandle eventWaitHandle;
+
+        public SingleInstanceHelper( string appGuid, Action showWindowAction, Action shutdownAction )
+        {
+            try
+            {
+                eventWaitHandle = EventWaitHandle.OpenExisting( appGuid );
+                eventWaitHandle.Set();
+
+                shutdownAction();
+                throw new AnotherInstanceOpenException();
+            }
+            catch( WaitHandleCannotBeOpenedException )
+            {
+                // No instance found, create a new one
+                eventWaitHandle = new EventWaitHandle( false, EventResetMode.AutoReset, appGuid );
+            }
+
+            // Watch for the handle to be signaled (by another instance starting up and create/open main window
+            new Task( () => {
+                while( eventWaitHandle.WaitOne() )
+                    Application.Current.Dispatcher.BeginInvoke( showWindowAction );
+            } ).Start();
+        }
+
+        public void Dispose() => eventWaitHandle.Close();
+    }
+
+    public static partial class JsonSerializerExtensions
+    {
+        public static T? DeserializeAnonymousType<T>( string json, T _, JsonSerializerOptions? options = default )
+            => JsonSerializer.Deserialize<T>( json, options );
+    }
+}
---
+++ WindowPositionFixer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Windows.Threading;
+
+namespace ChromeWindowFix
+{
+    internal class WindowPositionFixer
+    {
+        private Func<Process, bool> processSelector;
+        private Func<Rectangle, Rectangle, bool> windowNeedsFixSelector;
+        private Func<Rectangle, Rectangle> fixWindow;
+
+        private DispatcherTimer timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds( 1 ) };
+
+        public WindowPositionFixer( Func<Process, bool> processSelector, Func<Rectangle, Rectangle, bool> windowNeedsFixSelector,
+            Func<Rectangle, Rectangle> fixWindow )
+        {
+            this.processSelector = processSelector;
+            this.windowNeedsFixSelector = windowNeedsFixSelector;
+            this.fixWindow = fixWindow;
+
+            timer.Tick += (s,e) => FixWindows();
+            timer.Start();
+        }
+
+        private void FixWindows()
+        {
+            SystemParametersInfo( SystemParameters.SPI_GETWORKAREA, 0, out RECT tmp, 0 );
+            var desktopRect = new Rectangle( tmp.Left, tmp.Top, tmp.Right - tmp.Left, tmp.Bottom - tmp.Top );
+
+            foreach( var hwnd in Process.GetProcesses().Where( p => processSelector( p ) ).Select( p => p.MainWindowHandle ) )
+            {
+                DwmGetWindowAttribute( hwnd, DwmWindowAttribute.DWMWA_EXTENDED_FRAME_BOUNDS, out tmp, Marshal.SizeOf( typeof( RECT ) ) );
+                var windowRect = new Rectangle( tmp.Left, tmp.Top, tmp.Right - tmp.Left, tmp.Bottom - tmp.Top );
+
+                if( windowNeedsFixSelector( windowRect, desktopRect ) )
+                    SetWindow( hwnd, windowRect, fixWindow( windowRect ) );
+            }
+        }
+
+        public static void SetWindow( IntPtr hwnd, Rectangle originalRect, Rectangle newRect )
+        {
+            GetWindowRect( hwnd, out RECT originalWithShadow );
+
+            var shadow = new RECT {
+                Left = originalWithShadow.Left - originalRect.Left,
+                Right = originalWithShadow.Right - originalRect.Right,
+                Top = originalWithShadow.Top - originalRect.Top,
+                Bottom = originalWithShadow.Bottom - originalRect.Bottom
+            };
+
+            int width = (newRect.Right + shadow.Right) - (newRect.Left + shadow.Left);
+            int height = (newRect.Bottom + shadow.Bottom) - (newRect.Top - shadow.Top);
+
+            SetWindowPos( hwnd, IntPtr.Zero, newRect.Left + shadow.Left, newRect.Top + shadow.Top, width, height, 0x400 );
+        }
+
+        [DllImport( "dwmapi.dll" )]
+        static extern int DwmGetWindowAttribute( IntPtr hWnd, DwmWindowAttribute dwAttribute, out RECT lpRect, int cbAttribute );
+        
+        [StructLayout( LayoutKind.Sequential )]
+        public struct RECT { public int Left, Top, Right, Bottom; }
+
+        public enum DwmWindowAttribute { DWMWA_EXTENDED_FRAME_BOUNDS = 9 }
+
+        [DllImport( "user32.dll" )]
+        static extern bool SystemParametersInfo( SystemParameters uiAction, uint uiParam, out RECT pvParam, uint fWinIni );
+
+        enum SystemParameters { SPI_GETWORKAREA = 0x0030 };
+
+        [DllImport( "user32.dll" )]
+        internal static extern void GetWindowRect( IntPtr hwnd, out RECT lpRect );
+
+        [DllImport( "user32.dll" )]
+        internal static extern void SetWindowPos( IntPtr hwnd, IntPtr hwndInsertAfter, int X, int Y, int nWidth, int nHeight, uint flags );
+    }
+}
